eb5ce049c0d5cd36790d0af7901957a1f144468b
zt0454 <33329929@qq.com>
2014-05-09 01:56:38 +0800

    Delete MerchantDao.java

--- art/art-module/art-module-merchant/art-module-merchant-service/src/main/java/cn/culturemall/art/business/dao/merchant/MerchantDao.java
+++
@@ -1,8 +0,0 @@
-package cn.culturemall.art.business.dao.merchant;
-
-import cn.culturemall.art.business.domain.merchant.Merchant;
-import cn.culturemall.art.common.dao.CrudDao;
-
-public interface MerchantDao extends CrudDao<Long, Merchant> {
-
-}
